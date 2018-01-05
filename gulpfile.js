05a14636373af619a972afaa912f5f6bcf7df5a5
Adam <alspdx@gmail.com>
2018-01-05 08:33:03 -0800

    install gulp-concat and gulp-uglify, create gulp task to concat interface js files before jsBrowserify task and new task to minify scripts after jsBrowserify task is completed for production script files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/.js'] })
+gulp.task('concatInterface', function() {
+  return gulp.src(['./src/js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'] function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
-    .pipe(gulp.dest('./dist/js'));  
+    .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('minifyScripts', ['jsBrowserify'], function() {
+  return gulp.src('./dist/js/app.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
 });
