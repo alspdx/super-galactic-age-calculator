68c54b8b24ac363b539c0e9cd4119a682b37f859
Adam <alspdx@gmail.com>
2018-01-05 08:22:20 -0800

    install browserify and vinyl-source-stream and import into gulpfile, write jsBrowserify gulp task to generate browser ready script files from source script files

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('jsBrowserify', function() {
+  return browserify({ entries: ['./js/.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./dist/js'));  
+});
